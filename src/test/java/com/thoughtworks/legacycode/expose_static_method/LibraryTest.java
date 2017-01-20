2b8541fab76ee14c158dc17f8168dbbe22f17c13
Matthew Todd <matthew.todd@fluentsoftware.co.uk>
2017-01-20 14:36:53 +0000

    PowerMock example of the expose static method exercise

--- src/test/java/com/thoughtworks/legacycode/expose_static_method/LibraryTest.java
+++ src/test/java/com/thoughtworks/legacycode/expose_static_method/LibraryTest.java
@@ -1,13 +1,21 @@
 package com.thoughtworks.legacycode.expose_static_method;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertThat;
+import static org.powermock.api.support.membermodification.MemberMatcher.constructor;
+import static org.powermock.api.support.membermodification.MemberModifier.suppress;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Library.class)
 public class LibraryTest {
 //  Expose Static Method
 
@@ -20,6 +28,7 @@ public class LibraryTest {
         books.add(book);
 
         // Test will pause here while waiting for user input
+        suppress(constructor(Library.class, Console.class));
         Library library = new Library(System.console());
 
         // If overdueBooks was static we wouldn't have to create a new library
